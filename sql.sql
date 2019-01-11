7f38944ac7950283eb7d96fe48288833dc743fc5
RIHAB KASIM <rihabkasim.rk@gmail.com>
2019-01-12 02:35:46 +0530

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,16 @@
+show databases;
+create database rihab;
+use rihab;
+
+ create table stu(name varchar(20),age int);
+  insert into stu values("rihab",20),("kasim",50);
+  select * from stu;
+  
+ 
+
+
+
+ 
+
+ 
+ 
